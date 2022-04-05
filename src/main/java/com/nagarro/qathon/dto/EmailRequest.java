2d683c5c56140e949b480701eda59b59a476797d
practicelen <yogesh10sainanijune@gmail.com>
2022-04-05 18:47:50 +0530

    new branch;

--- src/main/java/com/nagarro/qathon/dto/EmailRequest.java
+++ src/main/java/com/nagarro/qathon/dto/EmailRequest.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EmailRequest {
 
+    //this is email property
     private String toEmail;
     private String emailSubject;
     private String emailBody;
